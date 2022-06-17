531ad635e9444414f00c6943612a686f00acf0be
nairAnhoJ <malondrasarian@gmail.com>
2022-06-17 23:23:17 +0800

    Minor Changes

--- admin/report-sales.php
+++ admin/report-sales.php
@@ -10,125 +10,122 @@
 
     $fDate = date("F j, Y", strtotime($getFromDate));
     $tDate = date("F j, Y", strtotime($getToDate));
-
-    require_once '../dompdf/vendor/autoload.php';
-    use Dompdf\Dompdf;
-
-    $html = '<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../bootstrap-5.1.3/css/bootstrap.min.css">
-        <title>Stock Report</title>
-    
-        <script src="../js/jquery-3.6.0.min.js"></script>
-        <script src="../bootstrap-5.1.3/js/bootstrap.bundle.min.js"></script>
-    
-        <style>
-            body{
-                padding: 0 0px;
-            }
-            h2{
-                font-weight: 900;
-                font-size: 25px;
-                padding: 0;
-                margin: 0;
-            }
-            h3{
-                font-weight: 900;
-                font-size: 16px;
-                padding: 0;
-                margin: 10px 0 20px 0;
-                display: inline-block;
-                width: 33%
-            }
-            h4{
-                font-weight: 900;
-                font-size: 16px;
-                padding: 0;
-                margin: 10px 0 20px 0;
-                display: inline-block;
-                text-align: center;
-                width: 33%
-            }
-            h5{
-                font-weight: 900;
-                font-size: 16px;
-                padding: 0;
-                margin: 10px 0 20px 0;
-                display: inline-block;
-                text-align: right;
-                width: 33%
-                background-color: red;
-            }
-            table{
-                width: 100%;
-                border-collapse: collapse;
-            }
-    
-            td,th{
-                border: 1px solid #444;
-                padding: 5px 20px;
-            }
-            th{
-                text-align: left;
-                font-size: 14px;
-            }
-            td{
-                font-size: 12px;
-            }
-            td:nth-child(2),td:nth-child(3),td:nth-child(4),th:nth-child(2),th:nth-child(3),th:nth-child(4){
-                width: 120px;
-                text-align: center;
-            }
-        </style>
-    </head>
-    <body>
-    
-        <h2>SALES REPORT</h2>
-        <div>
-            <h3>Date: '.$fDate.' - '.$tDate.'</h3>
-            <h4>'.$_SESSION['branch_name'].'</h4>
-            <h5>Location: '.$_SESSION['branch_loc'].'</h5>
-        </div>
-        <table>
-            <thead>
-                <th>Date</th>
-                <th>Total Sales (P)</th>
-            </thead>
-            <tbody>';
-
-                $grandTotal = 0;
-
-                $querySales ="SELECT SUM(`tran_total`) AS total, DATE(`tran_date_time`) AS thisDate FROM (SELECT DISTINCT(`tran_num`),`tran_total`,`tran_date_time` FROM `transaction_logs` WHERE `tran_date_time` BETWEEN '$dateFrom' AND '$dateTo') AS SelTran GROUP BY DATE(`tran_date_time`)";
-                $resultSales = mysqli_query($con, $querySales);
-                if(mysqli_num_rows($resultSales) > 0){
-                    while($rowSales = mysqli_fetch_assoc($resultSales)){
-                        $grandTotal = $grandTotal + $rowSales['total'];
-              $html .= '<tr>
-                            <td>'.date("F j, Y", strtotime($rowSales['thisDate'])).'</td>
-                            <td>'.number_format($rowSales['total'],2).'</td>
-                        </tr>';
-                    }
-                }
-
-    $html .= '  <tr>
-                    <td style="font-size: 18px; font-weight: 900; text-align: right;">GRAND TOTAL</td>
-                    <td style="font-size: 18px; font-weight: 900;">'.number_format($grandTotal,2).'</td>
-                </tr>
-            </tbody>
-        </table>
-    </body>
-    </html>';
-
-    $dompdf = new Dompdf();
-    $dompdf->loadHtml($html);
-    $dompdf->setPaper('A4', 'landscape');
-    $dompdf->render();
-    $dompdf->stream('low-stock-report.pdf', ['Attachment' => 0]);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap-5.1.3/css/bootstrap.min.css">
+    <title>Sales Report</title>
+
+    <script src="../js/jquery-3.6.0.min.js"></script>
+    <script src="../bootstrap-5.1.3/js/bootstrap.bundle.min.js"></script>
+
+    <style>
+        @page { 
+            size: auto;  margin: 0mm;  
+        }
+        body{
+            padding: 50px 50px;
+        }
+        h2{
+            font-weight: 900;
+            font-size: 25px;
+            padding: 0;
+            margin: 0;
+        }
+        h3{
+            font-weight: 900;
+            font-size: 16px;
+            padding: 0;
+            margin: 10px 0;
+        }
+        h4{
+            font-weight: 900;
+            font-size: 16px;
+            padding: 0;
+            margin: 10px 0;
+        }
+        h5{
+            font-weight: 900;
+            font-size: 16px;
+            padding: 0;
+            margin: 10px 0 20px 0;
+        }
+        table{
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        td,th{
+            border: 1px solid #444;
+            padding: 5px 20px;
+        }
+        th{
+            text-align: left;
+            font-size: 14px;
+            min-width: 150px;
+        }
+        td{
+            font-size: 12px;
+            min-width: 150px;
+        }
+        td:nth-child(2),td:nth-child(3),td:nth-child(4),th:nth-child(2),th:nth-child(3),th:nth-child(4){
+            width: 120px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body onload="reportPrint()">
+
+    <h2>SALES REPORT</h2>
+    <div>
+        <h3>Date: <?php echo $fDate; ?> - <?php echo $tDate; ?></h3>
+        <h4><?php echo $_SESSION['branch_name']; ?></h4>
+        <h5>Location: <?php echo $_SESSION['branch_loc']; ?></h5>
+    </div>
+    <table>
+        <thead>
+            <th>Date</th>
+            <th>Total Sales (P)</th>
+        </thead>
+        <tbody>
+            <?php
+
+            $grandTotal = 0;
+
+            $querySales ="SELECT SUM(`tran_total`) AS total, DATE(`tran_date_time`) AS thisDate FROM (SELECT DISTINCT(`tran_num`),`tran_total`,`tran_date_time` FROM `transaction_logs` WHERE `tran_date_time` BETWEEN '$dateFrom' AND '$dateTo') AS SelTran GROUP BY DATE(`tran_date_time`)";
+            $resultSales = mysqli_query($con, $querySales);
+            if(mysqli_num_rows($resultSales) > 0){
+                while($rowSales = mysqli_fetch_assoc($resultSales)){
+                    $grandTotal = $grandTotal + $rowSales['total'];
+                    ?>
+                        <tr>
+                            <td><?php echo date("F j, Y", strtotime($rowSales['thisDate'])); ?></td>
+                            <td><?php echo number_format($rowSales['total'],2); ?></td>
+                        </tr>
+                    <?php
+                }
+            }
+        ?>
+
+            <tr>
+                <td style="font-size: 18px; font-weight: 900; text-align: right;">GRAND TOTAL</td>
+                <td style="font-size: 18px; font-weight: 900;"><?php echo number_format($grandTotal,2); ?></td>
+            </tr>
+        </tbody>
+    </table>
+
+    <script>
+        function reportPrint(){
+            window.print();
+        }
+    </script>
+</body>
+</html>
 
 
         
--- admin/stock-report.php
+++ admin/stock-report.php
@@ -2,128 +2,111 @@
     session_start();
     date_default_timezone_set("Asia/Manila");
     include("../db/conn.php");
+?>
 
-    require_once '../dompdf/vendor/autoload.php';
-    use Dompdf\Dompdf;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap-5.1.3/css/bootstrap.min.css">
+    <title>Stock Report</title>
 
-    $html = '<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../bootstrap-5.1.3/css/bootstrap.min.css">
-        <title>Stock Report</title>
-    
-        <script src="../js/jquery-3.6.0.min.js"></script>
-        <script src="../bootstrap-5.1.3/js/bootstrap.bundle.min.js"></script>
-    
-        <style>
-            body{
-                padding: 0 0px;
-                width: 100vw;
-            }
-            h2{
-                font-weight: 900;
-                font-size: 25px;
-                padding: 0;
-                margin: 0;
-            }
-            div{
-                width: 100%;
-            }
+    <script src="../js/jquery-3.6.0.min.js"></script>
+    <script src="../bootstrap-5.1.3/js/bootstrap.bundle.min.js"></script>
+    <style>
+        @page { 
+            size: auto;  margin: 0mm;  
+        }
+        body{
+            padding: 50px 50px;
+        }
+        h2{
+            font-weight: 900;
+            font-size: 25px;
+            padding: 0;
+            margin: 0;
+        }
+        h3{
+            font-weight: 900;
+            font-size: 16px;
+            padding: 0;
+            margin: 10px 0;
+        }
+        h4{
+            font-weight: 900;
+            font-size: 16px;
+            padding: 0;
+            margin: 10px 0;
+        }
+        h5{
+            font-weight: 900;
+            font-size: 16px;
+            padding: 0;
+            margin: 10px 0 20px 0;
+        }
+        table{
+            width: 100%;
+            border-collapse: collapse;
+        }
 
-            h3{
-                font-weight: 900;
-                font-size: 16px;
-                padding: 0;
-                margin: 10px 0 20px 0;
-                display: inline-block;
-                width: 33%
-                background-color: blue;
-            }
-            h4{
-                font-weight: 900;
-                font-size: 16px;
-                padding: 0;
-                margin: 10px 0 20px 0;
-                display: inline-block;
-                text-align: center;
-                width: 33%
-            }
-            h5{
-                font-weight: 900;
-                font-size: 16px;
-                padding: 0;
-                margin: 10px 0 20px 0;
-                display: inline-block;
-                text-align: right;
-                width: 33%
-                background-color: red;
-            }
-            table{
-                width: 100%;
-                border-collapse: collapse;
-            }
-    
-            td,th{
-                border: 1px solid #444;
-                padding: 5px 20px;
-            }
-            th{
-                text-align: left;
-                font-size: 14px;
-            }
-            td{
-                font-size: 12px;
-            }
-            td:nth-child(2),td:nth-child(3),td:nth-child(4),th:nth-child(2),th:nth-child(3),th:nth-child(4){
-                width: 120px;
-                text-align: center;
-            }
-        </style>
-    </head>
-    <body>
-    
-        <h2>LOW STOCK REPORT</h2>
-        <div>
-            <h3>Date: '.date('F j, Y').'</h3>
-            <h4>'.$_SESSION['branch_name'].'</h4>
-            <h5>Location: '.$_SESSION['branch_loc'].'</h5>
-        </div>
-        <table>
-            <thead>
-                <th>Item Description</th>
-                <th>Category</th>
-                <th>Remaining Stock</th>
-                <th>Supplier</th>
-            </thead>
-            <tbody>';
+        td,th{
+            border: 1px solid #444;
+            padding: 5px 20px;
+        }
+        th{
+            text-align: left;
+            font-size: 14px;
+        }
+        td{
+            font-size: 12px;
+        }
+        td:nth-child(2),td:nth-child(3),td:nth-child(4),th:nth-child(2),th:nth-child(3),th:nth-child(4){
+            width: 120px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body onload="reportPrint()">
 
+    <h2>LOW STOCK REPORT</h2>
+    <div>
+        <h3>Date: <?php echo date('F j, Y'); ?></h3>
+        <h4> <?php echo $_SESSION['branch_name']; ?></h4>
+        <h5>Location: <?php $_SESSION['branch_loc']; ?></h5>
+    </div>
+    <table>
+        <thead>
+            <th>Item Description</th>
+            <th>Category</th>
+            <th>Remaining Stock</th>
+            <th>Supplier</th>
+        </thead>
+        <tbody>
+            <?php
                 $safeStock = $_SESSION['safe_stock'];
-                $queryLowStock = "SELECT `itemnb_name`,`itemnb_category`,`itemnb_stock`,`itemnb_suppplier` FROM `item_no_barcode` WHERE `itemnb_stock` < '$safeStock' UNION SELECT `item_name`,`item_category`,`item_stock`,`item_supplier` FROM `item_with_barcode` WHERE `item_stock` < '$safeStock'";
+                $queryLowStock = "SELECT itemnb_name,`itemnb_category`,`itemnb_stock`,`itemnb_suppplier` FROM item_no_barcode WHERE itemnb_stock < '$safeStock' UNION SELECT item_name,`item_category`,`item_stock`,`item_supplier` FROM item_with_barcode WHERE item_stock < '$safeStock'";
                 $resultLowStock = mysqli_query($con, $queryLowStock);
                 if(mysqli_num_rows($resultLowStock) > 0){
                     while($rowStock = mysqli_fetch_assoc($resultLowStock)){
-              $html .= '<tr>
-                            <td>'.ucwords($rowStock['itemnb_name']).'</td>
-                            <td>'.ucwords($rowStock['itemnb_category']).'</td>
-                            <td>'.$rowStock['itemnb_stock'].'</td>
-                            <td>'.ucwords($rowStock['itemnb_suppplier']).'</td>
-                        </tr>';
+                        ?>
+                            <tr>
+                                <td> <?php echo ucwords($rowStock['itemnb_name']); ?></td>
+                                <td> <?php echo ucwords($rowStock['itemnb_category']); ?></td>
+                                <td> <?php echo $rowStock['itemnb_stock']; ?></td>
+                                <td> <?php echo ucwords($rowStock['itemnb_suppplier']); ?></td>
+                            </tr>
+                        <?php
                     }
                 }
-    $html .= '</tbody>
-        </table>
-    </body>
-    </html>';
-
-    $dompdf = new Dompdf();
-    $dompdf->loadHtml($html);
-    $dompdf->setPaper('A4', 'landscape');
-    $dompdf->render();
-    $dompdf->stream('low-stock-report.pdf', ['Attachment' => 0]);
-?>
-
-
-        
+            ?>
+        </tbody>
+    </table>
+    <script>
+        function reportPrint(){
+            window.print();
+        }
+    </script>
+</body>
+</html>
